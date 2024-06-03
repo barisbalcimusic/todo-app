65a9bb7c9d2989dfbd06900e88fd4cfa728bce56
Baris <barisbalcimusic@gmail.com>
2024-06-03 21:31:15 +0200

    update created

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import DeleteNote from "./DeleteNote";
 import UpdateNote from "./UpdateNote";
+import { useNotesContext } from "../contexts/NotesContext";
 
 const NoteItem = ({ id, title, content }) => {
+  const [titleValue, setTitle] = useState(title);
+  const [contentValue, setContent] = useState(content);
   const [editMode, setEditMode] = useState(false);
+  const { setNotes } = useNotesContext();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,11 +21,12 @@ const NoteItem = ({ id, title, content }) => {
           },
           body: JSON.stringify({
             id: id,
-            title: title,
-            content: content,
+            title: titleValue,
+            content: contentValue,
           }),
         });
         const data = await res.json();
+        console.log(data);
         setNotes(data);
       } catch (error) {
         console.log(error);
@@ -35,20 +40,22 @@ const NoteItem = ({ id, title, content }) => {
       <form className="p-2 flex items-center gap-5" onSubmit={handleSubmit}>
         <div className="flex flex-col">
           <input
-            disabled={!editMode}
+            value={titleValue}
+            onChange={(e) => setTitle(e.target.value)}
             className={`p-2 font-bold  ${
               editMode ? "border-solid border-2 border-black-500" : ""
             }`}
             type="text"
-            defaultValue={title}
+            disabled={!editMode}
           />
           <input
+            value={contentValue}
+            onChange={(e) => setContent(e.target.value)}
             disabled={!editMode}
             className={`p-2 ${
               editMode ? "border-solid border-2 border-black-500" : ""
             }`}
             type="text"
-            defaultValue={content}
           />
         </div>
         <div className="flex gap-2">
