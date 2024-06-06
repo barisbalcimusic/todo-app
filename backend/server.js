dc3c62cccdf7e9bc67f7d5eeb4a36a3fefbb132e
Baris <barisbalcimusic@gmail.com>
2024-06-06 22:59:38 +0200

    env variables added

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import { notesRouter } from "./routers/notesRouter.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
+dotenv.config();
+
 app.use(cors());
 app.use(express.json());
 
-const port = 3000;
+const port = process.env.PORT;
 
 //ROUTES
 app.use("/api/notes", notesRouter);
