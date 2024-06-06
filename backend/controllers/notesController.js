9b150506f11383e7431fb66f6a2adc9e52372c4a
Baris <barisbalcimusic@gmail.com>
2024-06-06 22:52:25 +0200

    fs writeFileSync added

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -1,15 +1,31 @@
-let db = [];
+import { log } from "console";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dbPath = path.join(__dirname, "../db/db.json");
+
+let db = JSON.parse(fs.readFileSync(dbPath, "utf8"));
 
 export const getAllNotes = (req, res, next) => {
-  res.json(db);
+  try {
+    res.json(db);
+  } catch (e) {
+    next(e);
+  }
 };
 
 export const createNote = (req, res, next) => {
   try {
-    db.push(req.body);
+    const post = { id: new Date().getTime(), ...req.body };
+    db.push(post);
+    fs.writeFileSync(dbPath, JSON.stringify(db), "utf8");
     res.json(db);
+    console.log(db);
   } catch (e) {
-    res.json({ message: error.message });
+    next(e);
   }
 };
 
@@ -20,6 +36,7 @@ export const updateNote = (req, res, next) => {
       const { title, content } = req.body;
       const filteredNote = db.find((note) => Number(note.id) === Number(id));
       Object.assign(filteredNote, { title, content });
+      fs.writeFileSync(dbPath, JSON.stringify(db), "utf8");
       res.json(db);
     } else {
       res.status(404).json({ message: "Note not found" });
@@ -34,6 +51,7 @@ export const deleteNote = (req, res, next) => {
     const id = req.params.id;
     if (db.find((note) => Number(note.id) === Number(id))) {
       db = db.filter((note) => Number(note.id) !== Number(id));
+      fs.writeFileSync(dbPath, JSON.stringify(db), "utf8");
       res.json(db);
     } else {
       res.status(404).json({ message: "Note not found" });
