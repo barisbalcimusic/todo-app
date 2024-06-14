6b5d0827f1dfa50e9a71297ef68d7a5361e4d3ea
Baris <barisbalcimusic@gmail.com>
2024-06-14 21:24:32 +0200

    some styling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
+import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import NotesContextProvider from "./contexts/NotesContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
